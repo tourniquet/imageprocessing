ec88cb11a663d502776151a973b6318b97cc6f53
tourniquet <admyn3d@gmail.com>
2016-04-12 20:18:51 +0000

    some results

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -11,14 +11,27 @@ var sharp = require('sharp')
 
 module.exports = {
   upload (req, res) {
+    var images = path.resolve(sails.config.appPath + '/assets/images')
     req.file('form-first-image').upload({
-      dirname: path.resolve(sails.config.appPath + '/assets/images')
+      dirname: images
     }, function (err, files) {
-      if (err) {
-        console.log(err)
-      }
+      if (err) console.log(err)
 
-      console.log(files[0].fd)
+      var oldPath = files[0].fd
+      var index = oldPath.lastIndexOf('/') + 1
+      var path = oldPath.substr(index)
+      var newPath = '/assets/images/' + path
+
+      console.log(files)
+
+      // var newFile = path.resolve(sails.config.appPath)
+      // console.log(path.resolve(sails.config.appPath))
+
+      sharp(files[0].fd)
+        .resize(800, 600)
+        .toFile(images + '/a.jpg', function (err) {
+          if (err) console.log(err)
+        })
 
       res.json({ file: files })
     })
